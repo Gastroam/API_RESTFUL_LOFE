37e7c7d53327ce3b84536b9c190c366a59ceaaf4
OscarLH29 <oscar_lopezh29@outlook.com>
2019-09-21 21:17:16 -0500

    Problema arreglado de las cors

--- public/index.php
+++ public/index.php
@@ -2,6 +2,11 @@
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Message\ResponseInterface as Response;
 
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET, POST ,OPTIONS, PUT');
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header('Content-Type: application/json');
+
 require '../vendor/autoload.php';
 require '../src/config/db.php'; // importamos nuestro gestor de bd para usarlo
 
